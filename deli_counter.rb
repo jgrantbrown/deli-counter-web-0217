bdc14cb69563e1df855f1aea83698f6cd3fbb891
jason brown <j.grant.brown@gmail.com>
2017-01-05 18:28:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,16 @@
 def line(katz_deli)
   if katz_deli.length<=0
     puts "The line is currently empty."
-  else
+else
     katz_deli.each.with_index(1) {|inline, index| puts "The line is currently: #{index}. #{inline}"}
   end
 end
 
 def take_a_number(katz_deli,newperson)
-if katz_deli.length<1
-  katz_deli.push(newperson)  puts "Welcome, #{newperson}. You are number 1. in line."}
-end
+if katz_deli.empty?
+  katz_deli.push(newperson)
+   puts "Welcome, #{katz_deli.first}. You are number 1 in line."
 else
-katz_deli.push(newperson).each.with_index(1) {|inline, index| puts "Welcome, #{inline}. You are number #{index} in line."}
-end
+katz_deli.push(newperson)
+puts "Welcome #{katz_deli.last}. You are number #{katz_deli.count} in line."}
 end
