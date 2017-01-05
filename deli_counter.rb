ce9f7e5ead9bb869a04d4dd013b8fd72d8dd88cd
jason brown <j.grant.brown@gmail.com>
2017-01-05 16:40:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,12 @@ def line(katz_deli)
   if katz_deli.length<=0
     puts "The line is currently empty."
   else
-    katz_deli.map.with_index(1) {|inline, index| "The line is currently: #{index}. #{inline}"}
+    katz_deli.each.with_index(1) {|inline, index| puts "The line is currently: #{index}. #{inline}"}
   end
+end
+
+def take_a_number(katz_deli,newperson)
+  katz_deli.push(newperson).each.with_index(1) {|inline, index| puts "welcome, #{inline}. You are number #{index} in line."}
+  
+
 end
