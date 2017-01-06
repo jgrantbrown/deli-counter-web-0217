0f48c9b520ac75638b5f7426b468e647a6304f8f
jason brown <j.grant.brown@gmail.com>
2017-01-06 01:15:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
 else
-    puts "The Line is currently:  #{katz_deli.each.with_index(1)}"
+    puts "The line is currently:  #{katz_deli.map.with_index()}"
   end
 end
 
